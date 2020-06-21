9fdcc6879deaf39831c98c361990f33e92d84fec
Eric Carboni <ericcarboni@gmail.com>
2020-06-21 12:02:23 -0400

    feat: add netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,9 +1,3 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 const tailwind = require('tailwindcss')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
@@ -41,6 +35,12 @@ module.exports = {
             create: true
           }
         }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
       }
     },
     {
